52985705f153f2a0689a4c22c4be5f190e3e101a
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 01:10:33 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+if (0) {
+  alert('Zero');
+} else {
+  alert('Nooo');
+}
+
+ผลลัพธ์ 
+Nooo
+เนื่องจากเงื่อนไขใน if statement เป็น falsy (เป็น 0) ดังนั้นบล็อค else จะถูกทำงานแทน ซึ่งจะ alert คำว่า 'Nooo' ออกมา
+ดังนั้นผลลัพธ์ที่ได้จากโค้ดนี้คือการ alert 'Nooo' ออกมา.
